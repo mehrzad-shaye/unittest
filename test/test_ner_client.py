3309f387963e4f8e5db1f6d83c2bce1eaca43b70
mehrzad-shaye <mehrzadshaye@gmail.com>
2023-10-04 21:25:20 +0330

    new ED

--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -19,38 +19,54 @@ class TestNerClient(unittest.TestCase):
         ents = ner.get_ents("Madison is a city in wisconsin")
         self.assertIsInstance(ents, dict)
 
-    def  test_get_ents_give_spacy_NORP_is_returned_serializes_to_Group(self):
-         model = NerModelTestDouble('eng')
-         doc_ents = [{'text': 'Lithuanian Fressinet', 'label_':'NORP'}]
-         model.returns_doc_ents(doc_ents)
-         ner = NamedEntityClient(model)
-         result = ner.get_ents('...')
-         expected_result = {'ents': [{'ent': 'Lithuanian Fressinet', 'label':'Group'}], 'html': ""}
-         self.assertListEqual(result['ents'], expected_result['ents'])
+    def test_get_ents_give_spacy_NORP_is_returned_serializes_to_Group(self):
+        model = NerModelTestDouble('eng')
+        doc_ents = [{'text': 'Lithuanian Fressinet', 'label_': 'NORP'}]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityClient(model)
+        result = ner.get_ents('...')
+        expected_result = {
+            'ents': [{'ent': 'Lithuanian Fressinet', 'label': 'Group'}], 'html': ""}
+        self.assertListEqual(result['ents'], expected_result['ents'])
 
-    def  test_get_ents_give_spacy_LOC_is_returned_serializes_to_Location(self):
-         model = NerModelTestDouble('eng')
-         doc_ents = [{'text': 'the ocean', 'label_':'LOC'}]
-         model.returns_doc_ents(doc_ents)
-         ner = NamedEntityClient(model)
-         result = ner.get_ents('...')
-         expected_result = {'ents': [{'ent': 'the ocean', 'label':'Location'}], 'html': ""}
-         self.assertListEqual(result['ents'], expected_result['ents'])
-  
-    def  test_get_ents_give_spacy_LANGUAGE_is_returned_serializes_to_Language(self):
-         model = NerModelTestDouble('eng')
-         doc_ents = [{'text': 'ASL', 'label_':'LANGUAGE'}]
-         model.returns_doc_ents(doc_ents)
-         ner = NamedEntityClient(model)
-         result = ner.get_ents('...')
-         expected_result = {'ents': [{'ent': 'ASL', 'label':'Language'}], 'html': ""}
-         self.assertListEqual(result['ents'], expected_result['ents'])
+    def test_get_ents_give_spacy_LOC_is_returned_serializes_to_Location(self):
+        model = NerModelTestDouble('eng')
+        doc_ents = [{'text': 'the ocean', 'label_': 'LOC'}]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityClient(model)
+        result = ner.get_ents('...')
+        expected_result = {
+            'ents': [{'ent': 'the ocean', 'label': 'Location'}], 'html': ""}
+        self.assertListEqual(result['ents'], expected_result['ents'])
 
-    def  test_get_ents_give_spacy_GPE_is_returned_serializes_to_Location(self):
-         model = NerModelTestDouble('eng')
-         doc_ents = [{'text': 'Australia', 'label_':'GPE'}]
-         model.returns_doc_ents(doc_ents)
-         ner = NamedEntityClient(model)
-         result = ner.get_ents('...')
-         expected_result = {'ents': [{'ent': 'Australia', 'label':'Location'}], 'html': ""}
-         self.assertListEqual(result['ents'], expected_result['ents'])
+    def test_get_ents_give_spacy_LANGUAGE_is_returned_serializes_to_Language(self):
+        model = NerModelTestDouble('eng')
+        doc_ents = [{'text': 'ASL', 'label_': 'LANGUAGE'}]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityClient(model)
+        result = ner.get_ents('...')
+        expected_result = {
+            'ents': [{'ent': 'ASL', 'label': 'Language'}], 'html': ""}
+        self.assertListEqual(result['ents'], expected_result['ents'])
+
+    def test_get_ents_give_spacy_GPE_is_returned_serializes_to_Location(self):
+        model = NerModelTestDouble('eng')
+        doc_ents = [{'text': 'Australia', 'label_': 'GPE'}]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityClient(model)
+        result = ner.get_ents('...')
+        expected_result = {
+            'ents': [{'ent': 'Australia', 'label': 'Location'}], 'html': ""}
+        self.assertListEqual(result['ents'], expected_result['ents'])
+
+    def test_get_ents_give_multiple_ents_serializes_all(self):
+        model = NerModelTestDouble('eng')
+        doc_ents = [{'text': 'Australia', 'label_': 'GPE'},
+                    {'text': 'judith Polgar', 'label_': 'PERSON'}]
+        model.returns_doc_ents(doc_ents)
+        ner = NamedEntityClient(model)
+        result = ner.get_ents('...')
+        expected_result = {'ents':
+                    [{'ent': 'Australia', 'label': 'Location'},
+                    {'ent': 'judith Polgar', 'label': 'Person'}], 'html': ""}
+        self.assertListEqual(result['ents'], expected_result['ents'])
